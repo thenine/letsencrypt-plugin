a6e2706766ef9c1678ae133772cd2698de8fde17
Billy Swift <billyswifty@gmail.com>
2024-12-30 10:29:27 -0600

    le fix

--- lib/letsencrypt_plugin/certificate_output.rb
+++ lib/letsencrypt_plugin/certificate_output.rb
@@ -8,8 +8,9 @@ module LetsencryptPlugin
     def output
       display_info
 
-      output_cert('key.pem', @csr.private_key.to_pem)
-      output_cert('fullchain.pem', @certificate)
+     # output_cert('key.pem', @csr.private_key.to_pem)
+      output_cert('key.pem', @certificate.request.private_key.to_pem)
+      output_cert('fullchain.pem', @certificate.fullchain_to_pem)
     end
   end
 end
